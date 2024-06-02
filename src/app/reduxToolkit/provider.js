1291062d6c0513bf07d631584881c0c66f41c63e
Farzeen Ali <farzeenaliarif786@gmail.com>
2024-06-02 22:58:26 +0500

    Folder Structure, Store & Provider

---
+++ src/app/reduxToolkit/provider.js
@@ -0,0 +1,9 @@
+'use client'
+
+import { store } from "./store"
+
+const { Provider } = require("react-redux")
+
+export const Providers = ({children}) => {
+    return <Provider store={store}>{children}</Provider>
+}
